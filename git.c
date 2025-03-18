687a4d8e402474d7e3ba909f196d8aeeb01fa84a
rohit kumar <12rohit4@gmail.com>
2025-03-18 08:54:06 +0530

    adding git config chnages dones by c

---
+++ git.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(int argc, char *argv[]) {
+    if (argc != 3) {
+        fprintf(stderr, "Usage: %s <username> <email>\n", argv[0]);
+        return 1;
+    }
+
+    char *username = "rohitwtbs";
+    char *email = "12rohit4@gmail.com";
+
+    // Build the git config commands
+    char usernameCommand[256];
+    snprintf(usernameCommand, sizeof(usernameCommand), "git config --global user.name \"%s\"", username);
+
+    char emailCommand[256];
+    snprintf(emailCommand, sizeof(emailCommand), "git config --global user.email \"%s\"", email);
+
+    // Execute the git config commands using system()
+    int usernameResult = system(usernameCommand);
+    int emailResult = system(emailCommand);
+
+    // Check if the commands were executed successfully
+    if (usernameResult == 0 && emailResult == 0) {
+        printf("Git username and email updated successfully.\n");
+    } else {
+        fprintf(stderr, "Failed to update Git username or email.\n");
+        if(usernameResult != 0){
+            fprintf(stderr, "Username command failed. error code: %d\n", usernameResult);
+        }
+        if(emailResult != 0){
+            fprintf(stderr, "Email command failed. error code: %d\n", emailResult);
+        }
+        return 1;
+    }
+
+    return 0;
+}
